0ff40676b3beb639e92e0e75731f95efd5e42946
rolindaz <rolinda.zamboni@gmail.com>
2025-06-09 13:58:03 +0200

    initialize project with HTML, CSS, and JavaScript files for Simon Says game

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+
+*/
+
+// Visualizzare in pagina 5 numeri casuali
+
+// Da lì parte un timer di 30 secondi
+
+// Dopo 30 secondi i numeri scompaiono
+
+// appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
